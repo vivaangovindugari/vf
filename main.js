e008f465b02307c0b2c3be431c810dfb3ed2e0f9
vivaangovindugari <97267116+vivaangovindugari@users.noreply.github.com>
2022-10-19 15:55:26 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function bed()
+{
+    window.location = "bed.html";
+}
+function shoe()
+{
+    window.location = "shoe.html";
+}
+function laptop()
+{
+    window.location = "laptop.html";
+}
+function purse()
+{
+    window.location = "purse.html";
+}
+function desk()
+{
+    window.location = "desk.html";
+}
